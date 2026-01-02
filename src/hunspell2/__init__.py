f43d1b6d256da95ed39f99013d1017aa88f06fbd
Alejandro Rodriguez <alejandro.rodriguez@alexanderthamm.com>
2026-01-02 10:46:57 +0100

    fix: export from __init__

--- src/hunspell2/__init__.py
+++ src/hunspell2/__init__.py
@@ -1,2 +1,2 @@
-from hunspell2.cli_runner import CLIRunner
-from hunspell2.hunspell import HunSpell
+from hunspell2.cli_runner import CLIRunner as CLIRunner
+from hunspell2.hunspell import HunSpell as HunSpell
